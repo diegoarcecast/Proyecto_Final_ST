59f7695a35dd7d9a9a369757e3a79f886aee5223
Juan Diego Arce Castro <diegoarcecast@gmail.com>
2018-11-21 14:45:06 -0600

    Actualizacion 1

---
+++ BLL_Special_Ticket/BD/Cls_BD_BLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DAL_Special_Ticket.BD;
+using System.Configuration;
+
+namespace BLL_Special_Ticket.BD
+{
+    public class Cls_BD_BLL
+    {
+
+        public string Conectar_BD()
+        {
+            try
+            {
+                Cls_BD_DAL Obj_BD_DAL = new Cls_BD_DAL();
+
+                //por indice o por nombre
+                Obj_BD_DAL.SCadenaConec = ConfigurationManager.ConnectionStrings[0].ToString();
+                //creo obj conexion
+                Obj_BD_DAL.Obj_SQL_CNX = new SqlConnection(Obj_BD_DAL.SCadenaConec);
+                //abrir conex
+                if (Obj_BD_DAL.Obj_SQL_CNX.State == ConnectionState.Closed)
+                {
+                    Obj_BD_DAL.Obj_SQL_CNX.Open();
+                }
+
+
+                return string.Empty;
+            }
+            catch (Exception e)
+            {
+                return e.Message.ToString();
+
+            }
+        }
+    }
+}
---
+++ DAL_Special_Ticket/BD/Cls_BD_DAL.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL_Special_Ticket.BD
+{
+    public class Cls_BD_DAL
+    {
+        private string sMsjError, sCadenaConec;
+
+        public string SCadenaConec
+        {
+            get
+            {
+                return sCadenaConec;
+            }
+
+            set
+            {
+                sCadenaConec = value;
+            }
+        }
+
+        public string SMsjError
+        {
+            get
+            {
+                return sMsjError;
+            }
+
+            set
+            {
+                sMsjError = value;
+            }
+        }
+
+        public SqlConnection Obj_SQL_CNX;
+
+        public SqlDataAdapter Obj_SQL_DAP;
+
+        public SqlCommand Obj_SQL_CMD;
+
+    }
+}
