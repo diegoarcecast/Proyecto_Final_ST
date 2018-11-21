1b9db9975bb81f493ec4cfbf7dc12b496656304b
Juan Diego Arce Castro <diegoarcecast@gmail.com>
2018-11-21 14:31:21 -0600

    Montando la base

---
+++ DAL_Special_Ticket/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Special_Ticket
+{
+    public class Class1
+    {
+    }
+}
