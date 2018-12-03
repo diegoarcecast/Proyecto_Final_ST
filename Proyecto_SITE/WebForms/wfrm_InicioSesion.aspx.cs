869fd61f57d2c768662b79bd98a814d697e7f003
Nemesis Alfaro <nalfaroperez29@gmail.com>
2018-12-02 19:50:51 -0600

    Pagina Inicio Sesion

--- Proyecto_SITE/WebForms/wfrm_InicioSesion.aspx.cs
+++ Proyecto_SITE/WebForms/wfrm_InicioSesion.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Proyecto_SITE.WebForms
 {
-    public partial class wfrm_InicioSesion : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class wfrm_InicioSesion : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
